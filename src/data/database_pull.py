e0d7f585917b4c9b4cd45a3e648a263d0e0b8f02
bwu86 <bwu86@uwo.ca>
2021-05-08 17:33:41 -0400

    Added download_build_dataset.py to streamline data ingestion. Added an option to update existing dataset to reduce preprocessing time. TODO: Convert masking tool to TF 2.4 so that everything can be run in the same environment

--- src/data/database_pull.py
+++ src/data/database_pull.py
@@ -1,3 +1,4 @@
+
 import yaml
 import os
 import pandas as pd
@@ -6,32 +7,37 @@ import wget
 
 cfg = yaml.full_load(open(os.getcwd() + "/config.yml", 'r'))
 
-def data_pull():
+def data_pull(update):
     '''
     Pull raw clips from AWS database using a generated query in csv format
+    :update:    a boolean value, used to determine if the current dataset should be updated, or completely downloaded
+                affects the CSV_PATH variable
     '''
+
+    CSV_PATH=''
+
+    if update == '0':
+        CSV_PATH = 'CLIPS_TABLE'
+    elif update == '1':
+        CSV_PATH = 'UPDATED_CLIPS_TABLE'
+    else:
+        print("Unknown command, exiting program.")
+
     output_folder = cfg['PATHS']['RAW_CLIPS']
-    df = pd.read_csv(cfg['PATHS']['CLIPS_TABLE'])
-
+    df = pd.read_csv(cfg['PATHS'][CSV_PATH])
     print('Getting AWS links...')
-
     # Dataframe of all clip links
     links = df.s3_path
-
     print('Fetching clips from AWS...')
-
     # Download clips and save to disk
     for link in tqdm(links):
         print(link)
         firstpos = link.rfind("/")
         lastpos = len(link)
         filename = link[firstpos+1:lastpos]
-
         wget.download(link, output_folder + filename)
-
     print('Fetched clips successfully!')
-
     return
 
 if __name__ == '__main__':
-    data_pull()
+    data_pull(1)
--- src/data/query_to_df.py
+++ src/data/query_to_df.py
@@ -1,28 +1,36 @@
 import yaml
 import os
 import pandas as pd
+from tqdm import tqdm
+import wget
 
 cfg = yaml.full_load(open(os.getcwd() + "/config.yml", 'r'))
-
 COLUMNS_WANTED = ['patient_id', 'a_or_b_lines']
-
 database_query = cfg['PATHS']['DATABASE_QUERY']
 
-def create_ABline_dataframe(database_query):
+def create_ABline_dataframe(database_query, update):
     '''
     Extracts out pertinent information from database query csv and builds a dataframe linking filenames, patients, and class
     :database_filename: filepath to database query csv
+    :update:    a boolean value, used to determine if the current dataset should be updated, or completely downloaded
+                affects the CSV_PATH variable
     '''
-    df = pd.read_csv(database_query)
+    CSV_PATH=''
 
+    if update == '0':
+        CSV_PATH = 'CLIPS_TABLE'
+    elif update == '1':
+        CSV_PATH = 'UPDATED_CLIPS_TABLE'
+    else:
+        print("Unknown command, exiting program.")
+
+    df = pd.read_csv(database_query)
     # Remove all muggle clips
     df = df[df.frame_homogeneity.isnull()]
-
     df = df[df.a_or_b_lines != 'non_a,_non_b']
-
+    df = df[df.a_or_b_lines.notnull()]
     # Create filename
     df['filename'] = df['exam_id'] + "_" + df['patient_id'] + "_" + df["VID_id"]
-
     # Create column of class category to each clip. 
     # Modifiable for binary or multi-class labelling
     df['class'] = df.apply(lambda row: 0 if row.a_or_b_lines == 'a_lines' else
@@ -31,22 +39,15 @@ def create_ABline_dataframe(database_query):
                              (1 if row.a_or_b_lines == 'b_lines-_severe_(>50%_pleural_line)' else
                                2 if row.a_or_b_lines == 'non_a,_non_b' else
                                 -1))), axis=1)
-
     df['a_or_b_lines'] = df['a_or_b_lines'].replace({'b_lines_<_3': 'b_lines', 'b_lines-_moderate_(<50%_pleural_line)': 'b_lines', 'b_lines-_severe_(>50%_pleural_line)': 'b_lines'})
-
-    df['Path'] = df.apply(lambda row: '/home/derekwu/git repos/ab-line-classifer/data/masked_clips/' + row.filename, axis=1)
-
+    df['Path'] = df.apply(lambda row: '../../data/masked_clips/' + row.filename, axis=1)
     df['s3_path'] = df.apply(lambda row: row.s3_path, axis=1)
-    
     # Finalize dataframe
     df = df[['filename'] + COLUMNS_WANTED + ['class'] + ['Path'] + ['s3_path']]
-
     # Save df - append this csv to the previous csv 'clips_by_patient_2.csv'
-    df.to_csv(cfg['PATHS']['CLIPS_TABLE'], index=False)
-
-    return df
-
-#print(create_ABline_dataframe("parenchymal_clips.csv"))
+    df.to_csv(cfg['PATHS'][CSV_PATH], index=False)
+    return df, update
 
 if __name__ == "__main__":
-    create_ABline_dataframe(database_query)
+    create_ABline_dataframe(database_query, 1)
+    
